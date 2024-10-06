186b7967f3170f715d270828ef07e3ddaf3dd331
chen2021-web <83762621+chen2021-web@users.noreply.github.com>
2024-10-06 08:40:53 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # 修改默认IP
-sed -i 's/192.168.0.55/192.168.1.1/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.31.6/g' package/base-files/files/bin/config_generate
 
 # 更改默认 Shell 为 zsh
